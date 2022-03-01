f5a535fc276fe9dfaa6b3bf80b3089f992b1b81f
eti kona <etikonapal@gmail.com>
2022-03-02 00:51:02 +0600

    search box added

--- smart-shop.js
+++ smart-shop.js
@@ -1,1 +1,21 @@
-console.log('smart js joined with smart shop');
+const yourPhone =() =>{
+    const searchField = document.getElementById('search-field');
+    const searchFieldText = searchField.value;
+ console.log(searchFieldText);
+ const url = `https://openapi.programming-hero.com/api/phones?search=${searchFieldText}`;
+//  console.log(url);
+fetch(url)
+.then(res => res.json())
+.then(data => displayPhone(data.data))
+
+    searchField.value = '';
+}
+
+const displayPhone = data => {
+   const searchPhone = document.getElementById('search-phone');
+   data.forEach(phone => {
+       console.log(phone);
+       const div = document.createElement('div');
+       div.classList('col');
+   });
+}
