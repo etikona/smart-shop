5c7d0cbe8d714185095dba93f3cf71ea1735505a
eti kona <etikonapal@gmail.com>
2022-03-02 13:44:38 +0600

    phone details

--- smart-shop.js
+++ smart-shop.js
@@ -14,18 +14,27 @@ fetch(url)
 const displayPhone = data => {
    const searchPhone = document.getElementById('search-phone');
    data.forEach(phone => {
-       console.log(phone);
+      //  console.log(phone);
        const div = document.createElement('div');
        div.classList.add('col');
        div.innerHTML = `
-       <div class="card">
+       <div onclick=" phoneDetails('${phone.slug}')" class="col">
                 <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                  <h5 class="card-title">${phone.phone_name}</h5>
-                  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <h5 class="card-title">${phone.brand}</h5>
+                  <p class="card-text">${phone.phone_name}</p>
                 </div>
               </div>
        `;
        searchPhone.appendChild(div);
    });
 }
+const phoneDetails = phoneId =>{
+// console.log(phoneId);
+const url =`
+https://openapi.programming-hero.com/api/phone/${phoneId}
+`;
+fetch(url)
+.then(res => res.json())
+.then(data => console.log(data))
+};
